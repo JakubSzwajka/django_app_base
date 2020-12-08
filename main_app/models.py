bf5f9c9d481883c0e3b71607055858056dbef2bc
JakubSzwajka <szwajkajakub@gmail.com>
2020-12-08 14:01:28 +0100

    Navbar added

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 class Task(models.Model): 
     task_name = models.CharField(max_length=200)
     task_url = models.CharField(max_length=200)
-    
+    description = models.TextField(max_length=1000)
+
     def __str__(self):
         return self.task_name 
